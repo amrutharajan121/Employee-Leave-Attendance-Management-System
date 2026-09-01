eaf80629a16e0abcb5ac34af41c0b0ac492e3a44
amrutharajan121 <amrutharajanmallate@gmail.com>
2026-09-01 16:00:50 +0530

    Update CORS for frontend deployment

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      process.env.CLIENT_URL,
+      "https://employee-leave-attendance-managemen-ecru.vercel.app/"
     ],
     credentials: true,
   })
